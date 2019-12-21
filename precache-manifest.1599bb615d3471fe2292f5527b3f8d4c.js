4e501b5c9676fcf4886e905a4c4368f4773fde64
mastaBlasta <superad228@gmail.com>
2019-12-21 20:22:39 +0300

    Updates

---
+++ precache-manifest.1599bb615d3471fe2292f5527b3f8d4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3655227a9749196706f53090737290",
+    "url": "/mai-informatics/index.html"
+  },
+  {
+    "revision": "44f17ab2e83c867a40a1",
+    "url": "/mai-informatics/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f4da0f2a30fd5cca06db",
+    "url": "/mai-informatics/static/js/2.21f2a69e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mai-informatics/static/js/2.21f2a69e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44f17ab2e83c867a40a1",
+    "url": "/mai-informatics/static/js/main.65dd998c.chunk.js"
+  },
+  {
+    "revision": "744c7c98bbfbda74c73c",
+    "url": "/mai-informatics/static/js/runtime-main.8740ee72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mai-informatics/static/media/logo.5d5d9eef.svg"
+  }
+]);
